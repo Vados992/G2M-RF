97c4dfd37c24d65dfa03b45435b7e6a3a11cba01
Vados992 <zubilomolotok15@gmail.com>
2026-08-25 19:13:33 +0100

    Add geometry package exports

---
+++ src/g2mrf/geometry/__init__.py
@@ -0,0 +1,4 @@
+from .models import radius, physical_envelope, PHI, B_PHI
+from .fit import fit_model, fit_population_m4, reconstruct_m4
+
+__all__ = ["radius", "physical_envelope", "PHI", "B_PHI", "fit_model", "fit_population_m4", "reconstruct_m4"]
